0b0bd53df1abaa0550c8beda6fa9a85af9c79bf4
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-02-04 12:31:04 -0500

    The story Begins

---
+++ BurnsJoseph.js
@@ -0,0 +1,23 @@
+alert("The story Begins in the morning!!");
+console.log(" First I have to decide if today is a good day to run?" +
+" Is the weather right today?");
+
+
+var sunny = true, warm = true, rain = false, cold = false;
+
+if ((warm && cold) || rain) {
+    console.log(" It's time to run!!");
+}   else {
+    console.log("No running today if it's cold, and wet");
+}
+if (sunny && warm)  {
+    console.log("Perfect Day to Run if it's warm, and sunny!!");
+
+} else {
+    console.log("Maybe today's my off day!!!");
+}
+
+
+
+
+
