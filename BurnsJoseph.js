960199a94358fac365f6d1f949fc5d0610b8ef38
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-02-04 12:39:35 -0500

    I added loops, Strings w/ escapes

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -2,9 +2,8 @@ alert("The story Begins in the morning!!");
 console.log(" First I have to decide if today is a good day to run?" +
 " Is the weather right today?");
 
-
 var sunny = true, warm = true, rain = false, cold = false;
-
+// Below: Booleen Logic
 if ((warm && cold) || rain) {
     console.log(" It's time to run!!");
 }   else {
@@ -13,10 +12,50 @@ if ((warm && cold) || rain) {
 if (sunny && warm)  {
     console.log("Perfect Day to Run if it's warm, and sunny!!");
 
+// Below: Loops & Strings with escapes.
 } else {
     console.log("Maybe today's my off day!!!");
 }
+console.log("It's time to start a c2,5k training log" + " Week 4, day 3 involves:");
 
+ var warmUp = 5, jog = 6, walk = 2, jogAgain = 6, coolDown = 5;
+
+ while ( warmUp > 0 ) {
+    console.log( warmUp + " Minutes for warm up left!");
+ warmUp--
+ }
+ console.log("\"Run Now");
+
+ while ( jog > 0 ) {
+    console.log(jog + " minutes left");
+ jog--
+ }
+ console.log("\"Walk now\"");
+
+ while ( walk > 0 ) {
+    console.log(walk + " minutes until I jog again.");
+ walk--
+ }
+ console.log("\"Run Now\"");
+
+ while (jogAgain > 0 ) {
+    console.log(jogAgain + " Minutes left in my last run!!");
+ jogAgain--
+ }
+ console.log("\"Walk Now\"");
+
+ while (coolDown > 0) {
+    console.log( coolDown + " Minutes left in my cool down!!");
+ coolDown--
+ }
+ console.log("\" Workout: DONE !!!\"");
+
+ /*for ( warmUp = 0; warmUp < 5; warmUp++ 1) {
+ console.log (warmUp + " Minutes until I Jog.");
+ }
+ console.log("\"Jog Now.\"");  */
+
+// Below: Arrays
 
 
 
