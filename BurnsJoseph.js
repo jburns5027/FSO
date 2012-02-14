4e7f3af49c52040b85782b8374ec9013c07ede7f
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-02-13 20:59:48 -0800

    Boolean

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -1,109 +1,12 @@
- alert("The story Begins in the morning!!");
-console.log(" First I have to decide if today is a good day to run?" +
-" Is the weather right today?");
+alert("JavaScript works!");
 
-var sunny = true, warm = true, rain = false, cold = false;
+var passTest = true, flightsAvalible = true, failTest = false, flightsCanceled = false;
 // Below: Booleen Logic
-if ((warm && cold) || rain) {
-    console.log(" It's time to run!!");
+if ((passTest && flightsAvalible) || flightsCanceled) {
+    console.log("I can go to Califonia ");
 }   else {
-    console.log("No running today if it's cold, and wet");
+    console.log("I need to retake test!!");
 }
-if (sunny && warm)  {
-    console.log("Perfect Day to Run if it's warm, and sunny!!");
-
-// Below: Loops & Strings with escapes.
-} else {
-    console.log("Maybe today's my off day!!!");
+if (passTest && flightsAvalible)  {
+    console.log("I Finally Passed!!" + " Now I get to Go!!");
 }
-console.log("It's time to start a c2,5k training log" + " Week 4, day 3 involves:");
-
- var warmUp = 5, jog = 6, walk = 2, jogAgain = 6, coolDown = 5;
-
- while ( warmUp > 0 ) {
-    console.log( warmUp + " Minutes for warm up left!");
- warmUp--
- }
- console.log("\"Run Now");
-
- while ( jog > 0 ) {
-    console.log(jog + " minutes left");
- jog--
- }
- console.log("\"Walk now\"");
-
- while ( walk > 0 ) {
-    console.log(walk + " minutes until I jog again.");
- walk--
- }
- console.log("\"Run Now\"");
-
- while (jogAgain > 0 ) {
-    console.log(jogAgain + " Minutes left in my last run!!");
- jogAgain--
- }
- console.log("\"Walk Now\"");
-
- while (coolDown > 0) {
-    console.log( coolDown + " Minutes left in my cool down!!");
- coolDown--
- }
- console.log("\" Workout: DONE !!!\"");
-
- for ( warmUp = 0; warmUp < 5; warmUp+=1) {
- console.log (warmUp + " Minutes until I Jog.");
- }
- console.log("\"Jog Now.\"");
-
-// Below: Arrays
-
-var workOut = [ "upperbody", "off day", "lowerbody", "core", "cardio"],
-    workOutCount, lastWorkOutIndex;
-
-workOutCount = workOut.length;
-lastWorkOutIndex = workOutCount - 1;
-
-for (var i = 0; i < workOut.length; i++)  {
-    console.log("Work Out type: " + workOut[i]);
-}
-    console.log("Monday: " + workOut[0],
-               " Tuesday: " + workOut[1],
-               " Wedensday: " + workOut[2],
-               " Thursday: " + workOut[1],
-               " Friday: " + workOut[3],
-               " Saterday: " + workOut[1],
-               " Sunday: " + workOut[4]);
-workOut[3] = "RUN!!";
-console.log( workOut);
-
-/*workOutCount, lastworkOutIndex;
-
-workOutCounts = workOut.length;
-
-lastworkOutIndex = workOutCount -1; */
-
-// Below: Functions
-
-var stretchNames = ["back", "hamstring", "quads", "achilles"],
-    minsPerStretch = [2, 10, 5, 3];
-// Trying "Arguments"
-var stretchOneMuscle = function() {
-
-for (var stretchNumber = 0; stretchNumber < stretchNames.length; stretchNumber++) {
-     var stretchName = stretchNames[stretchNumber],
-        minsThisStretch = minsPerStretch[stretchNumber];
-
-    console.log("Starting to stretch the " + stretchName + " for " + minsThisStretch + " minutes");
-
-    for (var minutes = 0; minutes < minsThisStretch; minutes += 1) {
-        var minutesRemain = minsThisStretch - minutes;
-        console.log(minutes + " Done, " + minutesRemain + " to go! ");
-    }
-
-    }
-    console.log("I'm done with " + stretchName + ".");
-}
-    console.log("After work out stretches are important!!");
-
-
-
