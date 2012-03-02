90b7ec822b9b4d364c97a4fa0266c05134a90d24
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-03-02 11:43:03 -0500

    Logical Operators

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -1,67 +1,12 @@
-alert("JavaScript works!");
+// alert("JavaScript works!");
 
 
+var p = true, q = false, r = false;
 
-// String that fallows a pattern
-var a = true, b = true, c = true;
-if (a === true) {
-    console.log( "407-679-6333");
-    if (b === true) {
-        console.log ("phoneNumber");
-    }   else
-        console.log("Not a complete Phone Number");
-} else {
-    console.log ("Not a Full Number");
+if (!(p || q)) {
+    console.log("All true");
+
+}  else {
+    console.log("All False");
 }
-
-// Email
-
-var email = "sjburns"  + "5027" + "@fullsail.edu";
-
-//car email =  + "5027" + "@fullsail.edu";
-
-console.log(email);
-
-var  num = [1,2,3,4,5],
-     numCount, lastNumIndex;
-
-numCount = num.length;
-lastNumIndex = numCount -1;
-num.unshift(0);
-
-console.log(num[1]);
-// Numbers
-var a = [1,2,3,4];
-console.log([1] + [3]);
-
-
-/* var http = true; https = false, www = false;
-
-
-if ((http && https) || www) {
-    console.log(" It's time to run!!");
-}   else {
-    console.log("No running today if it's cold, and wet");
-
-}  */
-var https = true, www = false, http = false;
-// Below: Booleen Logic
-if ((https && www) || http) {
-    console.log("http");
-}   else {
-    console.log("https");
-
-}
-
-var a = 40;
-var b = 2;
-
-console.log(a + b);
-
-
-
-
-
-
-
 
