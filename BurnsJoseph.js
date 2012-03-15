1f8798bbdccbe61caa20fc7e003a30f8195a49ab
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-03-15 12:48:20 -0400

    Javascript week3

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -1,14 +1,24 @@
 alert("JavaScript works!");
+// JSON Data Below.
+var raceInfo = function (json) {
+    for (var i = 0; i < json.races.length; i++) {
+        var race = json.races[i];
+        console.log("Race Name: " + race.name + ", Race Number: " + race.number + ", Distance: " + race.distance + ", Location: " + race.location + ".");
+    }
+}
+
+raceInfo(json)
+
+
 
 // Procedure Below.
-
 var shoes = {
     brand: "nike",
     color: "black",
     minimualist: "yes",
 
     getShoes: function (cost) {
-        if ( cost <= 75) {
+        if (cost <= 75) {
             console.log("I'm going to buy new sneakers!!");
         } else {
             console.log("Way to expensive for me.");
@@ -17,8 +27,8 @@ var shoes = {
     }
 
 }
-console.log( shoes[ "color" ] );
-console.log( shoes [ "brand"]);
+console.log(shoes["color"]);
+console.log(shoes["brand"]);
 shoes.getShoes(70);
 
 
@@ -26,25 +36,15 @@ shoes.getShoes(70);
 
 
 // String Function Below.
-
 var bodyMuscles = {
-    upper: ["shoulders",
-            "biceps",
-            "triceps"
-    ],
-    core: ["abdomen",
-           "side abs",
-            "back"
-           ],
-    lower: ["thighs",
-            "calves",
-            "hamstrings"
-            ],
+    upper: ["shoulders", "biceps", "triceps"],
+    core: ["abdomen", "side abs", "back"],
+    lower: ["thighs", "calves", "hamstrings"],
 
     getBodySection: function (area) {
-          var bodyArea = "Today I'm working out the " + area + " part of my body.";
+        var bodyArea = "Today I'm working out the " + area + " part of my body.";
 
-        return bodyArea ;
+        return bodyArea;
     }
 }
 
@@ -57,18 +57,11 @@ console.log(chooseArea);
 
 
 // boolean functions.
-
- var outSideClothes = {
-     shirt: ["short Sleeve",
-             "dry fit material",
-             "white"],
-     legwear: [
-                "shorts",
-                "light cotton",
-                 "blue"
-              ],
-     chooseLocation: function(temp, weather ) {
-        if((temp == "hot") && (weather == "sunny")) {
+var outSideClothes = {
+    shirt: ["short Sleeve", "dry fit material", "white"],
+    legwear: ["shorts", "light cotton", "blue"],
+    chooseLocation: function (temp, weather) {
+        if ((temp == "hot") && (weather == "sunny")) {
             console.log("It's hot, and sunny.  Let's go running outside.");
             return true;
         } else {
@@ -79,10 +72,10 @@ console.log(chooseArea);
 
 }
 
- var outside = outSideClothes.chooseLocation("hot","sunny");
+var outside = outSideClothes.chooseLocation("hot", "sunny");
 //console.log(chooseLocation("hot","sunny"));
 //console.log(outside);
-if (outside === true){
+if (outside === true) {
     console.log("Let's take a run outside.");
 } else {
     console.log("Looks like the treadmill is in my future.");
@@ -94,69 +87,53 @@ if (outside === true){
 
 // Array Functions Bellow.
 var equipment = {
-       weight:"25",
-       material: "iron",
-       type: "dumbell",
+    weight: "25",
+    material: "iron",
+    type: "dumbell",
 
 
- workOutOneArea: function( workOut, minutesThisWorkOut){
-   var workOuts = [" Upper Body", "Lower Body", "Core"],
-         minutesPerWorkOut = [ 60, 60, 30 ];
+    workOutOneArea: function (workOut, minutesThisWorkOut) {
+        var workOuts = [" Upper Body", "Lower Body", "Core"],
+            minutesPerWorkOut = [60, 60, 30];
 
-    console.log("Starting to work out the" + workOut + " for "
-        + minutesThisWorkOut  + " minutes. ");
+        console.log("Starting to work out the" + workOut + " for " + minutesThisWorkOut + " minutes. ");
 
-    for (var minutes = 0; minutes < minutesThisWorkOut; minutes+= 10) {
-        var minutesRamain = minutesThisWorkOut - minutes;
-        console.log( minutes + " done, " + minutesRamain + " to Go!!");
+        for (var minutes = 0; minutes < minutesThisWorkOut; minutes += 10) {
+            var minutesRamain = minutesThisWorkOut - minutes;
+            console.log(minutes + " done, " + minutesRamain + " to Go!!");
 
-    }
-    console.log( "I'm done working out the " + workOut + "!!");
+        }
+        console.log("I'm done working out the " + workOut + "!!");
 
-},
-  /*
-    workOutAll:function ( workOuts, minutesPerWorkOut) {
-    for ( var workOutNumber = 0; workOutNumber < workOuts.length; workOutNumber++) {
-        var workOut = workOuts[workOutNumber],
-            minutesThisWorkOut = minutesPerWorkOut[workOutNumber];
-        workOutOneArea( workOut, minutesThisWorkOut );
+    },
 
-    }  */
+
 
 }
- var key = equipment.weight;
-console.log("Using the " + [ key ] + "LB weight.");
 
-equipment.workOutOneArea( " lower body", " 60");
+var key = equipment.weight;
+console.log("Using the " + [key] + "LB weight.");
+
+equipment.workOutOneArea(" lower body", " 60");
 // equipment.workOutAll( " lower body", " 60");
-
-
-
+var key = equipment.weight;
 
 
 // While Loop Function Bellow
 var c25k = {
-     platform:"IOS",
-     catagory: "fitness",
-     durationPerWorkOut:"30mins",
-     durationTotal: "9 months",
-     getCountDown: function(min, total) {
-            var runTime = min;
-            while ( min > 0) {
+    platform: "IOS",
+    catagory: "fitness",
+    durationPerWorkOut: "30mins",
+    durationTotal: "9 months",
+    getCountDown: function (min, total) {
+        var runTime = min;
+        while (min > 0) {
 
-                console.log (min + " Minutes left in my " + total + " minute workout");
-                min-=10;
-            }
-
+            console.log(min + " Minutes left in my " + total + " minute workout");
+            min -= 10;
         }
+
+    }
 }
 
 c25k.getCountDown(60, 90)
-
-
-
-
-
-
-
-
---
+++ json.js
@@ -0,0 +1,7 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Joe
+ * Date: 3/13/12
+ * Time: 12:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
