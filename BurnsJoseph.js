fc9a2edeecb85d6970c5738e69fcf7dcb440c52e
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-03-18 10:59:38 -0400

    Trying dates

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -29,3 +29,20 @@ var checkEmail = lib.getEmail( "jburns5027", "@", "fullsail.edu" )
 
 console.log( checkNumber );
 console.log( checkEmail);
+
+
+var foo = 10;
+var bar = 5;
+var today = new Date();
+var newdate = new Date (2012, 2, 18, 11, 11, 0 );
+var today2 = today.getDate();
+var today3 = today.getDate(22);
+
+
+
+console.log( today);
+console.log( newdate );
+console.log( newdate - today);
+console.log(today2);
+console.log(today3);
+console.log( foo - bar);
