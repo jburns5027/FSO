e68547e61f3330a816c18ee0f2e3fcf35027e502
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-03-19 17:42:45 -0400

    Email validation

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -9,10 +9,24 @@ return {
                     console.log( "Valid Phone Number.");
 
                 return phoneNumberPattern.test(elementValue);
+                }
+            },
+            getEmail: function validateEmail(elementValue) {
+                var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+                if (emailPattern.test(elementValue));{
+                    console.log("Valid Email.");
+                    return emailPattern.test(elementValue);
+                }  else {
+                    console.log
+                }
+
+
+
             }
 
 
 
+        }
 
 
 
@@ -25,9 +39,14 @@ var lib = jLib();
 var checkNumber = lib.getNumber( "404-679-0100" )
 var checkEmail = lib.getEmail( "jburns5027@fullsail.edu" )
 
-
-
 console.log( checkNumber );
 console.log( checkEmail);
 
-
+    /*
+    getEmail: function validateEmail(elementValue){
+        var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if (emailPattern.test(elementValue)); {
+            console.log(" Valid Email.")
+        }
+        return emailPattern.test(elementValue);
+        */
