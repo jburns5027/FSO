d6a569fb717ddb14e9d7c256472079d879823a3e
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-03-19 17:28:14 -0400

    Trying Validations

--- BurnsJoseph.js
+++ BurnsJoseph.js
@@ -2,28 +2,28 @@
 
 // String Fallowing a Phone Number below.
 var jLib = function() {
-       return {
+return {
+            getNumber: function validatePhoneNumber(elementValue){
+            var phoneNumberPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+                if (phoneNumberPattern.test(elementValue)); {
+                    console.log( "Valid Phone Number.");
 
-       getNumber: function (first, mid, last ) {
-                var validNumber = first + "-" + mid + "-" + last;
-
-            return validNumber;
-            },
-
-        getEmail: function (name, atsign, domain ) {
-            var validemail = "Valid E-Mail: " + name + atsign + domain;
-            return validemail;
-        }
+                return phoneNumberPattern.test(elementValue);
+            }
 
 
-       }
-}
+
+
+
+
+};
+
+
 
 
 var lib = jLib();
-
-var checkNumber = lib.getNumber("404", "679", "0100")
-var checkEmail = lib.getEmail( "jburns5027", "@", "fullsail.edu" )
+var checkNumber = lib.getNumber( "404-679-0100" )
+var checkEmail = lib.getEmail( "jburns5027@fullsail.edu" )
 
 
 
@@ -31,18 +31,3 @@ console.log( checkNumber );
 console.log( checkEmail);
 
 
-var foo = 10;
-var bar = 5;
-var today = new Date();
-var newdate = new Date (2012, 2, 18, 11, 11, 0 );
-var today2 = today.getDate();
-var today3 = today.getDate(22);
-
-
-
-console.log( today);
-console.log( newdate );
-console.log( newdate - today);
-console.log(today2);
-console.log(today3);
-console.log( foo - bar);
