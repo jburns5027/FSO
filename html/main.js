26f04d06924f8c2957068dfd492db9df8599f9ac
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-04-04 11:52:41 -0400

    ore js

--- html/main.js
+++ html/main.js
@@ -24,38 +24,58 @@ window.addEventListener("DOMContentLoaded" , function(){
    			makeOption.setAttribute("valure", optTest);
    			makeOption.innerHTML = optText;
    			makeSelect.appendChild(makeOption);
-   		
-   		
    		}
    		selectLi.appendChild(makeSelect);
    		getWorkOut();
+   	}	
+   		// Find the Value of a selected radio button.
+   		funtion getSelectedRadio() {
+   			var radios = document.form(0).yesorno
+   			for (var i=0; i<radio.length; i++) {
+   			if(radios[i].checked){
+   			yesornoValue = radios[i].value;
+   				
+   			}
+   		}   		
    }
+   function getCheckBoxValue() {
+   if($("yes").checked){
+   		yesValue =  $("yes").value;
+   }else{yesValue = "no"
+   }
+}
+   
    function  storeData(){
    		var id        		= Math.floor(Math.random()*1000000001);
    		// Gather all of our form field values and store in an object.
-   		// Object properties 
+   		// Object properties contain array with the form labeland input value.
+   		getSelectedRadio();
+   		getCheckBoxValue();
    		var item        	={};
-   		var item.hband		=["Headband", $(hband).value];
-   		var item.ttop		=["Tank Top, $(ttop).value];
-   		var item.tshirt		=["T-Shirt", $(tshirt).value];
-   		var item.ctop		=["Compression Top", $(ctop).value];
-   		var item.hoodie		=["Hoodie", $(hoodie).value];
-   		var item.cpants		=["Compression Pants", $(cpants).value];
-   		var item.dfshorts	=["Dry Fit Shorts", $(dfshorts).value];
-   		var item.csocks		=["Compression Socks", $(csock).value];
-   		var item.rshoes		=["Running Shoes", $(rshoes).value];
-   		var item.iphone		=["iPhone", $(iphone).value];
-   		var item.hphones	=["Head Phones", $(hphones).value];
-   		var item.hmonitor	=["Heartrate Monitor", $(hmonitor).value];
-   		var item.wbottle	=["Water Bottle", $(wbottle).value];
-   		var item.group		=["Group", $("groups").value];
-   		var item.completed	=["Completed:", $(completed).value];
-   		var item.dcompeted		=["Date Completed", $(dcompleted).value];
+   		var item.headband			=["Headband", $(headband).value];
+   		var item.tanktop			=["Tank Top, $(tanktop).value];
+   		var item.tshirt				=["T-Shirt", $(tshirt).value];
+   		var item.compressiontop		=["Compression Top", $(compressiontop).value];
+   		var item.hoodie				=["Hoodie", $(hoodie).value];
+   		var item.compressionpants	=["Compression Pants", $(compressionpants).value];
+   		var item.dryfitshorts		=["Dry Fit Shorts", $(dryfitshorts).value];
+   		var item.compressionsocks	=["Compression Socks", $(compressionsock).value];
+   		var item.runningshoes		=["Running Shoes", $(runningshoes).value];
+   		var item.iphone				=["iPhone", $(iphone).value];
+   		var item.headphones			=["Head Phones", $(headphones).value];
+   		var item.heartratemonitor	=["Heartrate Monitor", $(heartratemonitor).value];
+   		var item.fitnessapp			=["Fitness App", $(fitnessapp).value];
+   		var item.waterbottle		=["Water Bottle", $(waterbottle).value];
+   		var item.group				=["Group", $("groups").value];
+   		var item.yesorno			=["Completed:", $(completed).value];
+   		var item.dcompeted			=["Date Completed", $(dcompleted).value];
    		localStorage.setItem(id, JSON.stringify(item));
+   		alert("Workout Saved");
    }
    
    // variable defaults
    var workOutGroups = ["--Choose A Group--", "cardio", "weight", "core" ];
+   yesornoValue
    
    
    
