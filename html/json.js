57860f460cee1676701494170485b816d5358f32
Joe Burns <Joe@Joes-MacBook-Pro.local>
2012-04-23 15:42:51 -0400

    json

---
+++ html/json.js
@@ -0,0 +1,23 @@
+var json = {
+	"Workout1" : {
+		"apparel": 		   ["apparel:", "Compression Shirt", " Dry Fit Shirt", "Shorts", "Running Sneakers", "iPhone"  ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/22/2012" ],
+		"WoName":          ["WoName:", "Running" ]
+	},
+	"Workout2" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers"],
+		"group":           ["group:", "Core"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/20/2012" ],
+		"WoName":          ["WoName:", "Rock Climbing" ]  
+	},
+	"Workout3" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Weight"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/18/2012" ],
+		"WoName":          ["WoName:", "P90X" ]
+	}
+}
