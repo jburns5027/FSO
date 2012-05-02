69b9f75206bc36a8a9b6bb755c745954af7965c3
jburns5027 <jburns5027@fullsail.edu>
2012-05-02 14:56:34 -0400

    More dummy workouts

--- html/json.js
+++ html/json.js
@@ -20,4 +20,123 @@ var json = {
 		"datecompleted":   ["datecompleted", "4/18/2012" ],
 		"WoName":          ["WoName:", "P90X" ]
 	}
+	"Workout4" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Core"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/17/2012" ],
+		"WoName":          ["WoName:", "Sit-ups" ]
+	}
+	"Workout5" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle", "iPhone", "c25k app" ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/16/2012" ],
+		"WoName":          ["WoName:", "c25k app" ]
+	}
+	"Workouk6" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle","iPhone" ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "No" ],
+		"datecompleted":   ["datecompleted", "4/15/2012" ],
+		"WoName":          ["WoName:", "Marathon Training" ]
+	}
+	"Workout7" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Weight"],
+		"yesorno": 		   ["yesorno: ", "No" ],
+		"datecompleted":   ["datecompleted", "4/14/2012" ],
+		"WoName":          ["WoName:", "P90X" ]
+	}
+	"Workout8" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Core"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/14/2012" ],
+		"WoName":          ["WoName:", "Push-ups" ]
+	}
+	"Workout9" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle", "headband" ],
+		"group":           ["group:", "Weight"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/13/2012" ],
+		"WoName":          ["WoName:", "Jillian's Upperbody Workout" ]
+	}
+	"Workout10" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle", "iPhone" ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/13/2012" ],
+		"WoName":          ["WoName:", "Running" ]
+	}
+	"Workout11" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Core"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/12/2012" ],
+		"WoName":          ["WoName:", "Sit-ups" ]
+	}
+	"Workout12" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Weight"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/11/2012" ],
+		"WoName":          ["WoName:", "Pull-ups" ]
+	}
+	"Workout13" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/10/2012" ],
+		"WoName":          ["WoName:", "Cycling 20 Miles" ]
+	}
+	"Workout14" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "No" ],
+		"datecompleted":   ["datecompleted", "4/09/2012" ],
+		"WoName":          ["WoName:", "Cycling 35 Miles" ]
+	}
+	"Workout15" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Core"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/08/2012" ],
+		"WoName":          ["WoName:", "Sit-ups" ]
+	}
+	"Workout16" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Weight"],
+		"yesorno": 		   ["yesorno: ", "No" ],
+		"datecompleted":   ["datecompleted", "4/07/2012" ],
+		"WoName":          ["WoName:", "P90X" ]
+	}
+	"Workout17" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle", "iPhone", "c25k app" ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/06/2012" ],
+		"WoName":          ["WoName:", "c25k app" ]
+	}
+	"Workout18" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Core"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/05/2012" ],
+		"WoName":          ["WoName:", "Kettle Ball" ]
+	}
+	"Workout19" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Cardio"],
+		"yesorno": 		   ["yesorno: ", "No" ],
+		"datecompleted":   ["datecompleted", "4/04/2012" ],
+		"WoName":          ["WoName:", "Running 12 Miles" ]
+	}
+	"Workout20" : {
+		"apparel": 		   ["apparel:", " Dry Fit Shirt", "Shorts", "Running Sneakers", "Water Bottle" ],
+		"group":           ["group:", "Weight"],
+		"yesorno": 		   ["yesorno: ", "Yes" ],
+		"datecompleted":   ["datecompleted", "4/03/2012" ],
+		"WoName":          ["WoName:", "P90X" ]
+	}
 }
